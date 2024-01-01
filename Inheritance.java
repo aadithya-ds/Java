5d86a3022e139c0e5b35fb899a8ca200368ae5c1
aadithya-ds <aadithyaprasad.p@datasirpi.com>
2024-01-01 10:41:48 +0530

    inheritance

---
+++ Inheritance.java
@@ -0,0 +1,23 @@
+class Mobile {
+    void call() {
+        System.out.println("we can use call functionality");
+    }
+}
+
+class Nokia extends Mobile {
+
+    void camera() {
+        System.out.println("Nokia has 5MP camera");
+    }
+
+}
+
+public class Inheritane {
+
+    public static void main(String[] args) {
+        Nokia obj = new Nokia();
+        obj.call();
+        obj.camera();
+
+    }
+}
